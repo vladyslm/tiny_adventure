8221299fc99acd823c8ee18b2890699be090a879
Vladyslav M <vladislav.mpm@gmail.com>
2024-09-12 16:07:23 +0200

    add: SObjs for animation data and stats

--- Assets/App/Code/Scripts/ScriptableObjects/Animations.cs
+++ Assets/App/Code/Scripts/ScriptableObjects/Animations.cs
@@ -12,6 +12,9 @@ namespace TinyAdventure
         private const string JumpAnimation = "Jump";
         private const string DashAnimation = "Dash";
         private const string AttackAnimation = "Attack";
+        private const string FastAttackAnim = "Attack2";
+        private const string StabbingAttackAnim = "Attack3";
+        private const string SpinAttackAnim = "Attack4";
         private const string MovingAnimation = "MovingAttack";
 
         // Hashes
@@ -19,9 +22,17 @@ namespace TinyAdventure
         public readonly int JumpHash = Animator.StringToHash(JumpAnimation);
         public readonly int DashHash = Animator.StringToHash(DashAnimation);
         public readonly int AttackHash = Animator.StringToHash(AttackAnimation);
+        public readonly int FastAttackHash = Animator.StringToHash(FastAttackAnim);
+        public readonly int StabbingAttackHash = Animator.StringToHash(StabbingAttackAnim);
+        public readonly int SpinningAttackHash = Animator.StringToHash(SpinAttackAnim);
         public readonly int MovingAttackHash = Animator.StringToHash(MovingAnimation);
         
         // Animator Parameters
         public readonly int Speed = Animator.StringToHash("Speed");
+
+        [SerializeField] public float attack1Length = 0.25f;
+        [SerializeField] public float fastAttackLength = 0.26f;
+        [SerializeField] public float stabbingAttackLength = 0.26f;
+        [SerializeField] public float spinAttackLength = 1f;
     }
 }
