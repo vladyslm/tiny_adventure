ee19c3f8d38bff1495d9c1d1b0a0dc87bf2b3d50
Vladyslav M <vladislav.mpm@gmail.com>
2024-09-12 16:04:26 +0200

    add: run state class

---
+++ Assets/App/Code/Scripts/StateMachine/RunState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TinyAdventure
+{
+    public class RunState : BaseState
+    {
+        public RunState(PlayerController player, Animator animator) : base(player, animator)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Run State.OnEnter");
+            Animator.CrossFade(Player.Animations.LocomotionHash, 0);
+        }
+
+        public override void FixedUpdate()
+        {
+            Player.MovementController.HandleMovement(600);
+        }
+    }
+}
